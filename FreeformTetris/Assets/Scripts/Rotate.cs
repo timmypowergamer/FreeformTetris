e548214e002964d2d41ff18265917bd06fe0eb24
thepeterharrison <thepeterharrison@gmail.com>
2020-02-01 13:16:15 -0700

    Added Basic Title Screen

---
+++ FreeformTetris/Assets/Scripts/Rotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float rotationRate = 10f;
+    public Vector3 rotation = new Vector3(1, 1, 1);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotation, rotationRate * Time.deltaTime);
+    }
+}
