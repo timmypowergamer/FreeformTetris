416315750796fcdca0a6a7cde8b94d5509577446
thepeterharrison <thepeterharrison@gmail.com>
2020-02-02 12:09:09 -0700

    More art updates, music persists over scene change

---
+++ FreeformTetris/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private static AudioManager instance = null;
+    public static AudioManager Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- FreeformTetris/Assets/Scripts/PressAndHoldToSkip.cs
+++ FreeformTetris/Assets/Scripts/PressAndHoldToSkip.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PressAndHoldToSkip : MonoBehaviour
 {
+    public string TitleScene;
+    private float timeHeld = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,17 @@ public class PressAndHoldToSkip : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetButton("Submit") == true)
+        {
+            timeHeld += Time.deltaTime;
+            if (timeHeld > 3)
+            {
+                SceneManager.LoadScene(TitleScene);
+            }
+        }
+        else if (Input.GetButton("Submit") == false)
+            timeHeld = 0;
+
     }
 }
