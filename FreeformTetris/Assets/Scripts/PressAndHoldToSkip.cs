a170dde2e376fc83bdc5c4fe0ba17a250ce4a3ec
thepeterharrison <thepeterharrison@gmail.com>
2020-02-02 11:07:56 -0700

    Fixed Scoreboard again

--- FreeformTetris/Assets/Scripts/PressAndHoldToSkip.cs
+++ FreeformTetris/Assets/Scripts/PressAndHoldToSkip.cs
@@ -1,21 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
-
-public class ScoreboardScript : MonoBehaviour
+public class PressAndHoldToSkip : MonoBehaviour
 {
-    public int score;
     // Start is called before the first frame update
     void Start()
     {
-        GetComponentInChildren<TextMeshProUGUI>().text = score + "%";
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        GetComponentInChildren<TextMeshProUGUI>().text = score + "%";
+        
     }
 }
