bef36eb428710a1b861f9f2e842d40fde35811ea
thepeterharrison <thepeterharrison@gmail.com>
2020-02-01 21:15:04 -0700

    Added some further changes and improved Scoreboard

---
+++ FreeformTetris/Assets/Scripts/ScoreboardScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreboardScript : MonoBehaviour
+{
+    public int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponentInChildren<TextMesh>().text = score + "%";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponentInChildren<TextMesh>().text = score + "%";
+    }
+}
